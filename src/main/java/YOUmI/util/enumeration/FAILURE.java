bca0525f95010565d6fab1a1111f3bb031be5ea9
5onchangwoo <scw9410@gmail.com>
2023-04-27 21:58:05 +0900

    feat: 에러코드 추가
    - 자원 중복 에러코드 추가 (9997, DUPLICATE_RESOURCE)

--- src/main/java/YOUmI/util/enumeration/FAILURE.java
+++ src/main/java/YOUmI/util/enumeration/FAILURE.java
@@ -3,7 +3,8 @@ package YOUmI.util.enumeration;
 public enum FAILURE implements RESULT {
 
     FAILURE(FAILURE_CODE, "요청을 처리하지 못했습니다."),
-    ABSENT_REQUIRED_VALUE(ABSENT_REQUIRED_VALUE_CODE, "필수 값을 확인해주세요.");
+    ABSENT_REQUIRED_VALUE(ABSENT_REQUIRED_VALUE_CODE, "필수 값을 확인해주세요."),
+    DUPLICATE_RESOURCE(DUPLICATE_RESOURCE_CODE, "중복된 값이 포함되어 있습니다.");
 
     private final String code;
     private final String message;
