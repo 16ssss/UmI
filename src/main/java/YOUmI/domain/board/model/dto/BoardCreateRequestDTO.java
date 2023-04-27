2f475182dd3f99adf59d0e12c857be73d9dfa44a
5onchangwoo <scw9410@gmail.com>
2023-04-27 14:18:37 +0900

    doc: swagger 수정
    BoardCreateRequestDTO, BoardRepository swagger 수정

--- src/main/java/YOUmI/domain/board/model/dto/BoardCreateRequestDTO.java
+++ src/main/java/YOUmI/domain/board/model/dto/BoardCreateRequestDTO.java
@@ -5,12 +5,17 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @ToString
 public class BoardCreateRequestDTO {
+    @NotBlank
+    @Size(min = 2, max = 20)
     @Schema(description = "게시판 이름", example = "자유 게시판")
     private String boardName;
-    @Schema(description = "게시판 설명", example = "자유 게시판입니다.")
+    @Schema(description = "게시판 설명", example = "누구나 접근할 수 있는 게시판")
     private String boardDescribed;
 }
--- src/main/java/YOUmI/domain/board/repository/BoardRepository.java
+++ src/main/java/YOUmI/domain/board/repository/BoardRepository.java
@@ -2,9 +2,11 @@ package YOUmI.domain.board.repository;
 
 import YOUmI.domain.board.model.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
     Optional<Board> findByBoardNameAndDeletedYN(String boardName, char deletedYN);
 }
